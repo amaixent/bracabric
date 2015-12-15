101a84081072c3cbeb98c37b5a09b6f2cf1f7976
ShallaWincelslas <shallaworldxny@live.fr>
2015-12-15 12:34:27 +0100

    Modification de trajectoire.hpp et ajout du dossier shaders dans template/ projet

--- template/glimac/include/Core/Trajectoire.hpp
+++ template/glimac/include/Core/Trajectoire.hpp
@@ -1,4 +1,7 @@
 #pragma once
+#include <glimac/glm.hpp>
+
+using namespace glm;
 
 namespace glimac {
 
@@ -6,12 +9,18 @@ class Trajectoire
 {
 private:
 	int id;
+	float angle_actuel;
+	float position[];
+	int id_position_actuel;
+	float angle_zero[];
 	
 public:
 	Trajectoire();
 	~Trajectoire();
 
 	void seDeplacer();
+	mat3 pivotement(float angle);
+	mat3 position_suivante();
 
 	/* data */
 };
